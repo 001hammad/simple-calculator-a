e7f91ce50f2ad05943b93530149d1cdec3bdb7ef
001hammad <167647584+001hammad@users.noreply.github.com>
2024-11-13 16:17:03 +0500

    Update calculator.tsx

--- app/component/calculator.tsx
+++ app/component/calculator.tsx
@@ -33,21 +33,21 @@ function Calculator (){
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
           <h5 className='text-gray-500 mb-6'>Calculator is Developed By Hammad</h5>
-          <div className="bg-gray-200 p-6 rounded shadow-xl border border-gray-500 max-w-xs w-full">
+          <div className="bg-gray-200 p-5 rounded shadow-xl border border-gray-500 border-t-2 border-teal-500 max-w-xs w-full">
             <h2 className="text-xl font-bold mb-4">Simple Calculator</h2>
-            <div className="mt-4 text-lg font-bold">Result: {result}</div>
+            <div className="mt-4 text-lg font-bold bg-slate-300">Result: {result}</div>
             <input
               type="text"
               value={input}
               readOnly
-              className="w-full p-2 mb-4 border border-gray-300 rounded"
+              className="w-full p-4 mb-4 border border-gray-300 rounded"
             />
             <div className="grid grid-cols-4 gap-2 mb-4">
       {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'].map((btn) => (
         <button
           key={btn}
           onClick={() => (btn === '=' ? calcResult() : handleClick(btn))}
-          className={`p-2 text-white rounded hover:bg-opacity-80 
+          className={`p-2 text-white rounded-xl hover:bg-opacity-80 
             ${btn === '+' || btn === '-' || btn === '*' || btn === '/' || btn === '.' || btn === '=' ? 'bg-orange-500 hover:bg-orange-600' : 'bg-blue-500 hover:bg-blue-600'}`}
         >
           {btn}
@@ -57,7 +57,7 @@ function Calculator (){
     
             <button
               onClick={handleClear}
-              className="w-full p-2 bg-green-500 text-white rounded hover:bg-green-700"
+              className="w-full p-4 bg-green-500 text-white rounded-xl hover:bg-green-700 font-bold"
             >
               Clear
             </button>
